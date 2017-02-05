2d73ba4c4e9366fb4d666e2af6c63233e86d7139
Sisi Li <vivianss0301@gmail.com>
2017-02-04 19:41:28 -0500

    added content templates for each page

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,37 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next)=> {
  
-  res.render('index', { 
-    title: 'Home' 
-
+  res.render('content/index', { 
+    title: 'Home' ,
+    name:'SISI LI'
   });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('content/about', { title: 'About' });
 });
+
+/* GET projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', {
+    title: 'Projects'
+   });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', {
+    title: 'Services'
+   });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact'
+   });
+});
+
 
 module.exports = router;
